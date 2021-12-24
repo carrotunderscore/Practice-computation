a2ae98ce37774e83fae334b94e0f00a318ab3a3e
carrotunderscore <robertpergustafsson@gmail.com>
2021-12-24 09:07:24 +0100

    Can now ask 1-2-3 digit questions on the operators +, -, *.

--- app/src/main/java/com/example/practicecomputation/MainActivity.kt
+++ app/src/main/java/com/example/practicecomputation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.practicecomputation
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,16 +9,29 @@ import android.widget.EditText
 import android.widget.TextView
 import kotlin.random.Random
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.math.*
 
 class MainActivity : AppCompatActivity() {
+    var count: Int = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
+    fun startTimer(){
+        val timer = Timer()
+        val textCount: TextView = findViewById<TextView>(R.id.count)
+    }
+    fun resetCount(view: View){
+        count = 0;
+        val textCount: TextView = findViewById<TextView>(R.id.count)
+        textCount.text = "Count: " + count
+    }
+    /*------------------------------------------------------------------------------------------------------------------------*/
     fun singleDigitMultiplication(view: View) {
-        val randomNumber1 = (0..10).random()
-        val randomNumber2 = (0..10).random()
+        val randomNumber1 = (1..9).random()
+        val randomNumber2 = (1..9).random()
         val sum = randomNumber1 * randomNumber2
 
         val text: TextView = findViewById<TextView>(R.id.computation)
@@ -25,17 +39,26 @@ class MainActivity : AppCompatActivity() {
 
     }
     fun singleDigitAddition(view: View) {
-        val randomNumber1 = (0..10).random()
-        val randomNumber2 = (0..10).random()
+        val randomNumber1 = (1..9).random()
+        val randomNumber2 = (1..9).random()
         val sum = randomNumber1 + randomNumber2
 
         val text: TextView = findViewById<TextView>(R.id.computation)
         text.text = (String.format("%d", randomNumber1) + " + " + String.format("%d", randomNumber2));
     }
+    fun singleDigitSubtraction(view: View) {
+        val randomNumber1 = (1..9).random()
+        val randomNumber2 = (1..9).random()
+        val sum = randomNumber1 - randomNumber2
 
+        val text: TextView = findViewById<TextView>(R.id.computation)
+        text.text = (String.format("%d", randomNumber1) + " - " + String.format("%d", randomNumber2));
+    }
+
+    /*------------------------------------------------------------------------------------------------------------------------*/
     fun twoDigitMultiplication(view: View) {
-        val randomNumber1 = (10..100).random()
-        val randomNumber2 = (10..100).random()
+        val randomNumber1 = (10..99).random()
+        val randomNumber2 = (10..99).random()
         val sum = randomNumber1 * randomNumber2
 
         val text: TextView = findViewById<TextView>(R.id.computation)
@@ -43,33 +66,118 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun twoDigitAddition(view: View) {
-        val randomNumber1 = (10..100).random()
-        val randomNumber2 = (10..100).random()
+        val randomNumber1 = (10..99).random()
+        val randomNumber2 = (10..99).random()
         val sum = randomNumber1 + randomNumber2
 
         val text: TextView = findViewById<TextView>(R.id.computation)
         text.text = (String.format("%d", randomNumber1) + " + " + String.format("%d", randomNumber2));
     }
+    fun twoDigitSubtraction(view: View) {
+        val randomNumber1 = (10..99).random()
+        val randomNumber2 = (10..99).random()
+        val sum = randomNumber1 - randomNumber2
 
-    fun checkSum(view: View){
         val text: TextView = findViewById<TextView>(R.id.computation)
-        Log.d("H", text.text as String)
-        val answer: EditText = findViewById<EditText>(R.id.answer)
+        text.text = (String.format("%d", randomNumber1) + " - " + String.format("%d", randomNumber2));
+    }
 
-        if(text.text.contains("+")){
-            val textSplit = text.text!!.split("+").toTypedArray()
-            val num1 = textSplit[0].toInt()
-            val num2 = textSplit[1].toInt()
-            val additionSum = num1 + num2
-            Log.d("g", additionSum.toString())
+    /*------------------------------------------------------------------------------------------------------------------------*/
+    fun threeDigitMultiplication(view: View) {
+        val randomNumber1 = (100..999).random()
+        val randomNumber2 = (100..999).random()
+        val sum = randomNumber1 * randomNumber2
+
+        val text: TextView = findViewById<TextView>(R.id.computation)
+        text.text = (String.format("%d", randomNumber1) + " * " + String.format("%d", randomNumber2));
+    }
+
+    fun threeDigitAddition(view: View) {
+        val randomNumber1 = (100..999).random()
+        val randomNumber2 = (100..999).random()
+        val sum = randomNumber1 + randomNumber2
+
+        val text: TextView = findViewById<TextView>(R.id.computation)
+        text.text = (String.format("%d", randomNumber1) + " + " + String.format("%d", randomNumber2));
+    }
+    fun threeDigitSubtraction(view: View) {
+        val randomNumber1 = (100..999).random()
+        val randomNumber2 = (100..999).random()
+        val sum = randomNumber1 - randomNumber2
+
+        val text: TextView = findViewById<TextView>(R.id.computation)
+        text.text = (String.format("%d", randomNumber1) + " - " + String.format("%d", randomNumber2));
+    }
+
+    /*------------------------------------------------------------------------------------------------------------------------*/
+    @SuppressLint("SetTextI18n")
+    fun checkSum(view: View){
+
+        val textCount: TextView = findViewById<TextView>(R.id.count)
+        textCount.text = "Count: " + count
+
+        try{
+            val text: TextView = findViewById<TextView>(R.id.computation)
+            val answer: EditText = findViewById<EditText>(R.id.answer)
+            Log.d("H", "| Question: " + text.text as String)
+
+
+            if(text.text.contains("+")){
+                val computation = text.text as String
+                val textSplit = computation.split("+")
+
+                val additionSum : Float = textSplit[0].toFloat() + textSplit[1].toFloat()
+                if(answer.getText().toString() == additionSum.toInt().toString()){
+                    text.text = "Correctomundo"
+                    answer.setText("")
+                }
+                else{
+                    text.text = "Wrong; Answer was " + additionSum.toInt().toString()
+                    answer.setText("")
+                }
+                count++;
+                textCount.text = "Count: " + count
+            }
+
+
+            if(text.text.contains("*")){
+                val computation = text.text as String
+                val textSplit = computation.split("*")
+
+                val additionSum : Float = textSplit[0].toFloat() * textSplit[1].toFloat()
+                if(answer.getText().toString() == additionSum.toInt().toString()){
+                    text.text = "Correctomundo"
+                    answer.setText("")
+                }
+                else{
+                    text.text = "Wrong; Answer was " + additionSum.toInt().toString()
+                    answer.setText("")
+                }
+                count++;
+                textCount.text = "Count: " + count
+            }
+
+            if(text.text.contains("-")){
+                val computation = text.text as String
+                val textSplit = computation.split("-")
+
+                val additionSum : Float = textSplit[0].toFloat() - textSplit[1].toFloat()
+                if(answer.getText().toString() == additionSum.toInt().toString()){
+                    text.text = "Correctomundo"
+                    answer.setText("")
+                }
+                else{
+                    text.text = "Wrong; Answer was " + additionSum.toInt().toString()
+                    answer.setText("")
+                }
+                count++;
+                textCount.text = "Count: " + count
+            }
+
+        }catch(e: Exception){
 
         }
 
-        if(text.text.contains("*")){
-            val textSplit = text.text!!.split("*").toTypedArray()
-            val num1 = textSplit[0]
-            val num2 = textSplit[1]
-        }
 
 
 
